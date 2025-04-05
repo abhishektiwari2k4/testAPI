acde8a5fa412509b7e335359d50772d2ee020e56
abhishektiwari2k4 <61458016+abhishektiwari2k4@users.noreply.github.com>
2025-04-05 06:43:48 +0100

    Update BrowserStackAssignment.java

--- src/test/java/com/test/api/BrowserStackAssignment.java
+++ src/test/java/com/test/api/BrowserStackAssignment.java
@@ -11,6 +11,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 import java.time.Duration;
 import java.util.*;
 
@@ -30,6 +34,8 @@ public class BrowserStackAssignment {
         driver.manage().window().maximize() ;
         driver.get("https://elpais.com/");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(500));
+        
+        // Accept the cookie popup if present
         try{
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pmConsentWall-button")));
             WebElement acceptCookie = driver.findElement(By.className("pmConsentWall-button"));
@@ -37,9 +43,17 @@ public class BrowserStackAssignment {
         }catch (Exception e){
             System.out.println("Cookie accept button is not present.");
         }
+        
+        // Wait till subscribe link is present on screen in spanish
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='suscríbete']")));
+        // Click opinion
         WebElement opinion = driver.findElement(By.xpath("//div[@class='sm _df']/a[2]"));
         opinion.click();
+
+        /**
+         * Getting the first 5 titles and headers. If the article contains an image then that would be saved to local
+         * Storing the title values in list to translate later
+         */
         List<WebElement> section = driver.findElements(By.xpath("//section[@data-dtm-region='portada_apertura']/div"));
         List<String> title = new ArrayList<>();
         for(WebElement w: section){
@@ -48,12 +62,25 @@ public class BrowserStackAssignment {
                 title.add(ar.findElement(By.tagName("header")).findElement(By.tagName("h2")).getText());
                 System.out.println("Title is " + ar.findElement(By.tagName("header")).findElement(By.tagName("h2")).getText());
                 System.out.println("Content is " + ar.findElement(By.tagName("p")).getText());
+                String titleValue = ar.findElement(By.tagName("header")).findElement(By.tagName("h2")).getText();
+                try{
+                    String url = ar.findElement(By.tagName("figure")).findElement(By.tagName("a")).findElement(By.tagName("img")).getAttribute("src");
+                    System.out.println("Image url is " + url);
+                    URL imageURL = new URL(url);
+                    BufferedImage saveImage = ImageIO.read(imageURL);
+                    ImageIO.write(saveImage, "jpg", new File(titleValue + ".jpg"));
+                }catch (Exception e){
+                    System.out.println("No Image was found.");
+                }
             }
             if (title.size()>=5)
                 break;
         }
+        
+        // Using Google Translate API to translate the titles
         Map<String, String> headers = new HashMap<>();
         headers.put("X-RapidAPI-Key","8af4502a88msheeede4be4cfdad0p1457e7jsn3ef70b91eb29");
+        String h = "";
         for (String s: title) {
             String body = "{\"from\":\"auto\",\"to\":\"en\",\"text\":\"convert\"}";
             body = body.replace("convert", s);
@@ -62,6 +89,26 @@ public class BrowserStackAssignment {
             Response rs = given().contentType(ContentType.JSON).headers(headers).body(body).when().
                     post("https://google-translate113.p.rapidapi.com/api/v1/translator/text").then().extract().response();
             System.out.println(rs.jsonPath().getString("trans"));
+            h+= rs.jsonPath().getString("trans") + " ";
+        }
+        //Removing special characters from the combined title values which have been stored in a string. Then checking the
+        // frequency of these words.
+        h = h.replace(".", "").replace(",", "").replace("‘", "")
+                .replace("’", "").replace(":", "").replace("-", "").toLowerCase();
+        System.out.println(h);
+        String [] h1 = h.split(" ");
+        Map<String, Integer> m = new HashMap<>();
+        for(String s: h1){
+            if(m.containsKey(s)){
+                m.put(s, m.get(s)+1);
+            }
+            else
+                m.put(s,1);
+        }
+        for (Map.Entry<String, Integer> m1: m.entrySet()){
+            if(m1.getValue()>2){
+                System.out.println(m1.getKey());
+            }
         }
     }
 }
